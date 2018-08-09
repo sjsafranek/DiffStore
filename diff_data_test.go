012c5db3cee44661bb1e3c1f9c208690cdcd6571
stefan safranek <stefan@stefan.stefan>
2018-08-09 12:19:56 -0700

    fixed tests

--- diff_data_test.go
+++ diff_data_test.go
@@ -49,55 +49,11 @@ func BenchmarkGetCurrent(b *testing.B) {
 	}
 }
 
-// TestGetSnapshotsSuccess
-func TestGetSnapshotsSuccess(t *testing.T) {
-	generateTestRandomData("TestGetSnapshotsSuccess", 100)
-	snapshots := TestDiffStore.GetSnapshots()
-	if 0 == len(snapshots) {
-		t.Error("No timestamps found")
-	}
-}
-
-// BenchmarkGetSnapshots
-func BenchmarkGetSnapshots(b *testing.B) {
-	generateTestRandomData("BenchmarkGetSnapshots", 100)
-	// benchmark
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		TestDiffStore.GetSnapshots()
-	}
-}
-
-// TestGetPreviousByTimestampSuccess
-func TestGetPreviousByTimestampSuccess(t *testing.T) {
-	generateTestRandomData("TestGetPreviousByTimestampSuccess", 100)
-	snapshots := TestDiffStore.GetSnapshots()
-	idx := int(len(snapshots) / 2)
-	_, err := TestDiffStore.GetPreviousByTimestamp(snapshots[idx])
-	if nil != err {
-		t.Error(err)
-	}
-}
-
-// BenchmarkGetPreviousByTimestamp
-func BenchmarkGetPreviousByTimestamp(b *testing.B) {
-	// requirements
-	generateTestRandomData("BenchmarkGetPreviousByTimestamp", 100)
-	snapshots := TestDiffStore.GetSnapshots()
-	idx := int(len(snapshots) / 2)
-	// benchmark
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		TestDiffStore.GetPreviousByTimestamp(snapshots[idx])
-	}
-}
-
 // TestGetPreviousByIndexSuccess
 func TestGetPreviousByIndexSuccess(t *testing.T) {
 	// requirements
 	generateTestRandomData("TestGetPreviousByIndexSuccess", 100)
-	snapshots := TestDiffStore.GetSnapshots()
-	idx := int(len(snapshots) / 2)
+	idx := int(TestDiffStore.Length() / 2)
 	// test
 	_, err := TestDiffStore.GetPreviousByIndex(idx)
 	if nil != err {
@@ -109,40 +65,11 @@ func TestGetPreviousByIndexSuccess(t *testing.T) {
 func BenchmarkGetPreviousByIndex(b *testing.B) {
 	// requirements
 	generateTestRandomData("BenchmarkGetPreviousByIndex", 100)
-	snapshots := TestDiffStore.GetSnapshots()
-	idx := int(len(snapshots) / 2)
+	idx := int(TestDiffStore.Length() / 2)
 	// benchmark
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		TestDiffStore.GetPreviousByIndex(idx)
-	}
-}
-
-// TestGetPreviousWithinTimestampRangeSuccess
-func TestGetPreviousWithinTimestampRangeSuccess(t *testing.T) {
-	// requirements
-	generateTestRandomData("TestGetPreviousWithinTimestampRangeSuccess", 100)
-	snapshots := TestDiffStore.GetSnapshots()
-	idx1 := int(len(snapshots) / 5)
-	idx2 := int(len(snapshots) / 2)
-	// test
-	_, err := TestDiffStore.GetPreviousWithinTimestampRange(snapshots[idx1], snapshots[idx2])
-	if nil != err {
-		t.Error(err)
-	}
-}
-
-// BenchmarkGetPreviousWithinTimestampRange
-func BenchmarkGetPreviousWithinTimestampRange(b *testing.B) {
-	// requirements
-	generateTestRandomData("BenchmarkGetPreviousWithinTimestampRange", 100)
-	snapshots := TestDiffStore.GetSnapshots()
-	idx1 := int(len(snapshots) / 5)
-	idx2 := int(len(snapshots) / 2)
-	// benchmark
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		TestDiffStore.GetPreviousWithinTimestampRange(snapshots[idx1], snapshots[idx2])
 	}
 }
 
